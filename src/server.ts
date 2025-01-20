d7ba01049a5f573c64410be52746dcf9a712bccc
A1-mamun <1154almamun@gmail.com>
2025-01-20 12:45:42 +0600

    Database Connect

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
+import mongoose from 'mongoose';
 import app from './app';
-import 'dotenv/config';
+import config from './app/config';
 
 async function main() {
   try {
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => {
-      console.log(`App listening on port ${port}!`);
+    await mongoose.connect(config.database_url as string);
+
+    app.listen(config.port, () => {
+      console.log(`App listening on port ${config.port}!`);
     });
   } catch (error) {
     console.log(error);
